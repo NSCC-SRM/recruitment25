86a84b5373cef02b736059db1b3ec6516399c2ee
HarshilMalhotra <malhotraharshil2@gmail.com>
2024-07-20 19:56:17 +0530

    Created Footer for website. Added responsive design elements

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,70 @@
-export default function Navbar() {
-    return(
+import React from 'react';
+
+import NSCCLogo from "../assets/Footer/NSCC.png";
+
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
+
+export default function Footer() {
+    return (
         <>
-        <div className="bg-blue-600 text-white">Footer</div>
+            <div className='text-white bg-black md:bg-gradient-to-r from-[#124a4a] to-[#000000] flex flex-col lg:flex-row md:justify-between gap-y-5 font-roboto md:px-[160px] w-full pt-10 md:pt-0'>
+                
+                
+                <div className='flex flex-col justify-center items-center md:border-0 border-[4px] border-rt1 rounded-xl h-[266px] w-[264px] m-auto md:m-0'>
+                    <h1 className='text-rt1 text-[36px]'> Contact: </h1>
+
+                    <div className='flex flex-row items-center gap-2'>
+                        <FaPhoneAlt className='w-[38px] h-[38px]' />
+                        <div className='text-[24px]'>
+                            +91 1234567890<br />
+                            +91 1234567890
+                        </div>
+                    </div>
+
+                    <div className='flex align-items-center items-center gap-2'>
+
+                        <IoMdMail className='w-[43px] h-[40px]' />
+                        <a href="mailto:info@nsccsrm.tech" className='text-[24px]'>info@nsccsrm.tech</a>
+                    </div>
+                </div>
+
+
+                <div className='flex flex-col gap-y-4 md:w-[220px] items-center justify-center'>
+                    <h1 className='text-rt1 text-[36px]'> Follow Us on: </h1>
+                    <div className='flex gap-8 '>
+
+                       <a href="https://www.instagram.com/nscc_srm/?hl=en" target='_blank'> <FaInstagram className='w-[42px] h-[42px]' /></a>
+                       <a href="https://www.linkedin.com/company/newton-school-coding-club-srmist/?originalSubdomain=in" target='_blank'> <FaLinkedinIn className='w-[42px] h-[42px]' /></a>
+                       <a href="https://x.com/nsccsrm?lang=en" target='_blank'> <FaXTwitter className='w-[37px] h-[39px]' /></a>
+                    </div>
+                    <div className='box-border border-rt1 border-[3px] flex flex-row rounded-full h-[60px] w-[240px] gap-x-6'>
+                        <img src={NSCCLogo} alt="NSCC Logo" className='w-[70.91px] h-[52.59px]' />
+                        <p className='text-[18px]'>
+                            Developed by:<br />
+                            NSCC SRM
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className='invisible md:visible pt-8'>
+                    <h1 className='text-rt1 text-[36px] '> Links:</h1>
+                    <div className='flex flex-col text-[24px]'>
+                        <a href='#'>Home</a>
+                        <a href='#'>About Us</a>
+                        <a href='#'>Timeline</a>
+                        <a href='#'>Contact Us</a>
+
+                    </div>
+                </div>
+
+
+
+            </div>
         </>
-    )}
+    );
+}
