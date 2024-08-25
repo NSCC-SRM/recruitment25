1c2443dc2e9bcc5df7579e952db2b7fdd7c9606f
Punit <jpunit2005@gmail.com>
2024-08-25 17:56:00 +0530

    Finalizing details

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,21 +11,21 @@ import { FaPhoneAlt } from "react-icons/fa";
 export default function Footer() {
     return (
         <>
-            <div id='contact' className='text-white bg-black md:bg-gradient-to-r from-[#124a4a] to-[#000000] flex flex-col lg:flex-row md:justify-between md:content-center md:items-center gap-y-5 font-roboto md:px-[160px] w-full pt-10 md:pt-0 sm:h-[500px] py-12'>
+            <div id='contact' className='text-white bg-black md:bg-gradient-to-r from-[#124a4a] to-[#000000] flex flex-col md:flex-row md:justify-between md:content-center md:items-center gap-y-5 font-roboto md:px-[160px] w-full pt-10 md:pt-0 sm:h-[200px] py-12'>
 
 
-                <div className='flex flex-col justify-center items-center md:border-0 border-[4px] border-rt1 rounded-xl h-[266px] w-[264px] m-auto md:m-0'>
-                    <h1 className='text-rt1 text-[36px]'> Contact: </h1>
+                <div className='flex flex-col justify-center items-center md:border-0 border-[4px] border-rt1 rounded-xl h-[200px] w-[264px] m-auto md:m-0'>
+                    <h1 className='text-rt1 text-[28px]'> Contact: </h1>
 
                     <div className='flex align-items-center items-center gap-2'>
 
-                        <IoMdMail className='w-[43px] h-[40px]' />
-                        <a href="mailto:info@nsccsrm.tech" className='text-[24px]'>nscc@srmist.edu.in</a>
+                        <IoMdMail className='w-[32px] h-[30px]' />
+                        <a href="mailto:info@nsccsrm.tech" className='text-[18px]'>nscc@srmist.edu.in</a>
                     </div>
 
                     <div className='flex flex-row items-center gap-2'>
-                        <FaPhoneAlt className='w-[38px] h-[38px]' />
-                        <div className='text-[24px]'>
+                        <FaPhoneAlt className='w-[28px] h-[28px]' />
+                        <div className='text-[18px]'>
                             +91 98376 38743<br />
                             +91 99106 18509
                         </div>
@@ -34,17 +34,17 @@ export default function Footer() {
                 </div>
 
 
-                <div className='flex flex-col gap-y-4 md:w-[220px] items-center justify-center'>
-                    <h1 className='text-rt1 text-[34px]'> FOLLOW US:</h1>
+                <div className='flex flex-col gap-y-4 md:w-[220px] items-center justify-center pt-12'>
+                    <h1 className='text-rt1 text-[28px]'> Follow Us:</h1>
                     <div className='flex gap-8 '>
 
-                        <a href="https://www.instagram.com/nscc_srm/?hl=en" target='_blank'> <FaInstagram className='w-[42px] h-[42px]' /></a>
-                        <a href="https://www.linkedin.com/company/newton-school-coding-club-srmist/?originalSubdomain=in" target='_blank'> <FaLinkedinIn className='w-[42px] h-[42px]' /></a>
-                        <a href="https://x.com/nsccsrm?lang=en" target='_blank'> <FaXTwitter className='w-[37px] h-[39px]' /></a>
+                        <a href="https://www.instagram.com/nscc_srm/?hl=en" target='_blank'> <FaInstagram className='w-[32px] h-[32px]' /></a>
+                        <a href="https://www.linkedin.com/company/newton-school-coding-club-srmist/?originalSubdomain=in" target='_blank'> <FaLinkedinIn className='w-[32px] h-[32px]' /></a>
+                        <a href="https://x.com/nsccsrm?lang=en" target='_blank'> <FaXTwitter className='w-[28px] h-[30px]' /></a>
                     </div>
-                    <div className='box-border border-rt1 border-[3px] flex flex-row rounded-full h-[60px] w-[240px] gap-x-6'>
-                        <img src={NSCCLogo} alt="NSCC Logo" className='w-[70.91px] h-[52.59px]' />
-                        <p className='text-[18px]'>
+                    <div className='box-border border-rt1 border-[3px] flex flex-row rounded-full h-[60px] w-[190px] gap-x-3'>
+                        <img src={NSCCLogo} alt="NSCC Logo" className='w-[60px] h-[50px] pt-1' />
+                        <p className='text-[17px]'>
                             Developed by:<br />
                             NSCC SRM
                         </p>
@@ -53,12 +53,12 @@ export default function Footer() {
                 </div>
 
                 <div className='hidden lg:flex flex-col pt-8'>
-                    <h1 className='text-rt1 text-[36px] '> Links:</h1>
-                    <div className='flex flex-col text-[24px]'>
+                    <h1 className='text-rt1 text-[28px] '> Links:</h1>
+                    <div className='flex flex-col text-[18px]'>
                         <a href='#'>Home</a>
                         <a href='#community'>About Us</a>
                         <a href='#timeline'>Timeline</a>
-                        <a href='#contact'>Contact Us</a>
+                        <a href='#faq'>FAQs</a>
 
                     </div>
                 </div>
